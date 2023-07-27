3f695a37639155cd27e3bd4c3b605b72c5205c14
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-07-27 19:08:24 +0800

    update route about us

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -2,14 +2,14 @@
 
 import React from "react";
 
-const Landing = () => {
+const About = () => {
   return (
     <div className="flex h-[100vh] min-w-full content-center justify-center flex-col ">
       <div className="mx-auto text-[34px] font-medium animate-bounce font-bold">
-        LANDING SCREEN
+        About Us
       </div>
     </div>
   );
 };
 
-export default Landing;
+export default About;
