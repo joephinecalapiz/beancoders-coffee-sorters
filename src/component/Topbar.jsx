e71b6397220c85b7403be321fe8d677323f11189
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-07-30 23:46:36 +0800

    update

--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -73,7 +73,7 @@ const Topbar = () => {
   };
 
   return (
-    <div className="absolute flex flex-row w-full text-white text-[14px]">
+    <div className="absolute top-0 left-0 right-0 flex flex-row w-full text-white text-[14px]">
       <div className="bg-black h-full w-full flex items-center">
         <img src={BeansLogo} alt="BeansLogo" className="h-16 w-16 mt-1" />
         <h1 className="text-white text-16px ml-2">BeanCoders</h1>
