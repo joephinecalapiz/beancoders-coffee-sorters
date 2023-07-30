2b14d56714751b29c412cbabc34ffeef7302966e
Jeremiah Larry King Ungsod <j.ungsod019@gmail.com>
2023-07-30 15:03:12 +0800

    update

--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -12,7 +12,10 @@ const Topbar = () => {
   };
 
   return (
-    <div className="absolute flex flex-row w-full text-white text-[14px]">
+    <div
+      className="absolute flex flex-row w-full text-white text-[14px]"
+      style={{ position: "fixed", top: 0, left: 0, zIndex: 100 }}
+    >
       <div className="bg-black h-full w-full flex items-center">
         <img src={BeansLogo} alt="BeansLogo" className="h-16 w-16 mt-1" />
         <h1 className="text-white text-16px ml-2">BeanCoders</h1>
