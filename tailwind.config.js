aa219b5b496cc4f439c2b15be55afd3ab53dbc97
Alliana Marie <allianaonate@gmail.com>
2023-08-02 23:46:52 +0800

    improved register ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ export default {
       BgLanding: "url('./assets/bg_landing2.png')",
       bgopacity: "url('./assets/bgopacity.png')",
       bgLogin: "url('./assets/login_bg2.png')",
+      bgRegister: "url('./assets/reg_bg2.png')",
     },
   },
 
