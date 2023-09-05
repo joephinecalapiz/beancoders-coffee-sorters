8126be8a74533028f948f276395a6f642415f74c
EErickson <avilasonson@gmail.com>
2023-09-05 20:10:00 +0800

    fetching image done on landing page

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -6,6 +6,7 @@ import BeansLogo from "../assets/beansLogo.png";
 import Navbar from "../component/Navbar.jsx";
 import axios from "axios";
 import api_endpoint from "../config";
+import image_endpoint from "../image-config";
 const Landing = () => {
   const [authenticated, setAuthenticated] = useState(null);
   const [companyData, setCompanyData] = useState([]);
@@ -23,7 +24,13 @@ const Landing = () => {
   useEffect(() => {
     axios.get(api_endpoint + "/companies").then((response) => {
       const data = response.data;
-      setCompanyData(data.companies.map((company) => company.details[0]));
+      setCompanyData(data.companies.map((company) => {
+        const detail = company.details[0];
+        if (detail && detail.images) {
+          detail.images = detail.images.replace(/[\[\]\\\"]/g, '');
+        }
+        return detail;
+      }));
 
       console.log(companyData);
     });
@@ -38,49 +45,49 @@ const Landing = () => {
     return <div>Loading...</div>;
   }
 
-  const establishmentData = [
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    // Add more establishment objects here
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-    {
-      name: "The Coffee Haven",
-      address: "Makati City, Metro Manila",
-      phone: "123-456-7890",
-    },
-  ];
+  // const establishmentData = [
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   // Add more establishment objects here
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  //   {
+  //     name: "The Coffee Haven",
+  //     address: "Makati City, Metro Manila",
+  //     phone: "123-456-7890",
+  //   },
+  // ];
 
   return (
     <>
@@ -116,20 +123,25 @@ const Landing = () => {
                 key={index}
                 className="bg-gray-200 h-59 w-full md:w-74 rounded-lg p-4 border border-gray-300"
               >
-                <img
-                  src={BeansLogo}
-                  alt="beansLogo"
-                  className="h-25 w-25 mb-1 relative top-[-45px] md:left-0 md:mt-0"
-                />
+                {/* {console.log(detail.images )} */}
+                {detail && detail.images ? (
+                  <img
+                    src={`${image_endpoint}/storage/${detail.images}`}
+                    alt="beansLogo"
+                    className="h-25 w-25 mb-1 relative top-[-45px] md:left-0 md:mt-0"
+                  />
+                ) : (
+                  <div>No image available</div>
+                )}
                 {detail && (
                   <>
-                    <p className="text-black dark:text-textTitle font-bold relative top-[-70px] text-3xl font-poppins">
+                    <p className="text-white dark:text-textTitle font-bold relative top-[-70px] text-3xl font-poppins">
                       {detail.companyName}
                     </p>
-                    <p className="text-black dark:text-textDesc relative top-[-45px] text-3xl font-poppins">
+                    <p className="text-white dark:text-textDesc relative top-[-45px] text-3xl font-poppins">
                       {detail.companyLocation}
                     </p>
-                    <p className="text-black dark:text-textDesc relative top-[-20px] text-3xl font-poppins">
+                    <p className="text-white dark:text-textDesc relative top-[-20px] text-3xl font-poppins">
                       {detail.companyNumber}
                     </p>
                   </>
