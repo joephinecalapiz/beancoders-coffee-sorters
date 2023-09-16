4922de594dbe2e328f06866f4d4ddd38559b6859
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-09-16 19:19:42 +0800

    update topbar and sidebar in Profile page

--- src/screens/ProfileScreen/Profile.jsx
+++ src/screens/ProfileScreen/Profile.jsx
@@ -131,20 +131,20 @@ const Profile = () => {
     <>
       <div class="max-w-8xl mx-auto pl-16">
         <Sidebar collapsed={navVisible} handleToggleSidebar={toggleSidebar} />
-        <Topbar onToggleSidebar={toggleSidebar} />
+        <Topbar onToggleSidebar={toggleSidebar} collapsed={navVisible} handleToggleSidebar={toggleSidebar} />
         <div
           className={`Profile  ${navVisible ? "profile-shift-right" : ""}`}
           //  style={{ backgroundColor: '#d4d4d4' }}
         >
-          <div className={`p-5 pl-12 ${navVisible ? "ml-0" : "sm:ml-64"}`}>
+          <div className={`p-4 ${navVisible ? "ml-0" : "sm:ml-48"}`}>
             <div className="p-0.5 mb-16 w-full mt-6 relative">
               <h1 className="text-black bg-white mt-10 font-bold text-base p-3 rounded-lg shadow-xl">
                 Profile
               </h1>
             </div>
-            <div className={`p-5 ${navVisible ? "" : "ml-36"}`}>
-              <div class="max-w-2xl mx-auto bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
-                <div class="flex flex-col items-center justify-center ">
+            <div className={`p-4 ${navVisible ? "ml-0" : ""}`}>
+              <div className="max-w-2xl bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
+                <div className="flex flex-col items-center justify-center ">
                   <div className="top relative circular-profile object-none object-top overflow-visible">
                     <label
                       htmlFor="profilePicture"
@@ -173,9 +173,9 @@ const Profile = () => {
                   </label>
                 </div>
               </div>
-              <div class="max-w-2xl mt-5 mx-auto overflow-hidden lg:max-w-full">
-                <div class="grid grid-cols-1 sm:grid-cols-3 grid-rows-1">
-                  <div class="flex items-center justify-center sm:justify-self-start col-span-2">
+              <div className="max-w-2xl mt-5 overflow-hidden lg:max-w-full">
+                <div className="grid grid-cols-1 sm:grid-cols-3 grid-rows-1">
+                  <div className="flex items-center justify-center sm:justify-self-start col-span-2">
                     <h1
                       style={{
                         fontSize: "20px",
@@ -187,7 +187,7 @@ const Profile = () => {
                       {/* User Information */}
                     </h1>
                   </div>
-                  <div class="flex items-center justify-center sm:justify-self-end">
+                  <div className="flex items-center justify-center sm:justify-self-end">
                     {isEditing ? (
                       <div className="profile-edit-buttons">
                         <button
@@ -214,67 +214,67 @@ const Profile = () => {
                   </div>
                 </div>
               </div>
-              <div class="max-w-2xl mt-5 mx-auto overflow-hidden lg:max-w-full">
-                <div class="grid grid-cols-1 sm:grid-cols-3 grid-rows-1 gap-2">
-                  <div class="max-w-2xl sm:justify-self-start bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
-                    <div class="mobile:px-5 mt-5">
+              <div className="max-w-2xl mt-5 overflow-hidden lg:max-w-full">
+                <div className="grid grid-cols-1 sm:grid-cols-3 grid-rows-1 gap-2">
+                  <div className="max-w-2xl sm:justify-self-start bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
+                    <div className="mobile:px-5 mt-5">
                       <div>
-                        <div class="px-4 sm:px-0 align-center justify-center">
-                          <h3 class="text-base font-semibold leading-7 text-gray-900">
+                        <div className="px-4 sm:px-0 align-center justify-center">
+                          <h3 className="text-base font-semibold leading-7 text-gray-900">
                             Admin Information
                           </h3>
-                          <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+                          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
                             Admin details.
                           </p>
                         </div>
-                        <div class="mt-6 border-t border-gray-100">
-                          <dl class="divide-y divide-gray-100">
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900">
+                        <div className="mt-6 border-t border-gray-100">
+                          <dl className="divide-y divide-gray-100">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900">
                                 Full name
                               </dt>
                               {isEditing ? (
-                                <div class="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <div className="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                   <input
                                     type="text"
                                     name="name"
                                     id="name"
                                     autocomplete="name"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                     placeholder={profileData.name}
                                   />
                                 </div>
                               ) : (
-                                <dd class="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
+                                <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
                                   {profileData.name}
                                 </dd>
                               )}
                             </div>
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900">
                                 Role
                               </dt>
-                              <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 justify-self-end">
+                              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 justify-self-end">
                                 Admin
                               </dd>
                             </div>
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900">
                                 Email address
                               </dt>
                               {isEditing ? (
-                                <div class="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <div className="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                   <input
                                     type="text"
                                     name="email"
                                     id="email"
                                     autocomplete="email"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                     placeholder={profileData.email}
                                   />
                                 </div>
                               ) : (
-                                <dd class="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
+                                <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
                                   {profileData.email}
                                 </dd>
                               )}
@@ -284,36 +284,36 @@ const Profile = () => {
                       </div>
                     </div>
                   </div>
-                  <div class="max-w-2xl:mt-5 col-span-2 max-w-2xl bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
-                    <div class="mobile:px-5 mt-5">
+                  <div className="max-w-2xl:mt-5 col-span-2 max-w-2xl bg-white rounded-xl shadow-lg overflow-hidden lg:max-w-full">
+                    <div className="mobile:px-5 mt-5">
                       <div>
-                        <div class="px-4 sm:px-0 align-center justify-center">
-                          <h3 class="text-base font-semibold leading-7 text-gray-900">
+                        <div className="px-4 sm:px-0 align-center justify-center">
+                          <h3 className="text-base font-semibold leading-7 text-gray-900">
                             Company Information
                           </h3>
-                          <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+                          <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
                             Company details.
                           </p>
                         </div>
-                        <div class="mt-6 border-t border-gray-100">
-                          <dl class="divide-y divide-gray-100">
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900 align-center self-center">
+                        <div className="mt-6 border-t border-gray-100">
+                          <dl className="divide-y divide-gray-100">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900 align-center self-center">
                                 Company Name
                               </dt>
                               {isEditing ? (
-                                <div class="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <div className="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                   <input
                                     type="email"
                                     name="companyName"
                                     id="companyName"
                                     autocomplete="companyName"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                     placeholder={profileData.companyName}
                                   />
                                 </div>
                               ) : (
-                                <dd class="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
+                                <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
                                   {profileData.companyName}
                                 </dd>
                               )}
@@ -330,44 +330,44 @@ const Profile = () => {
                               <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 justify-self-end">{profileData.companyName}</dd>
                             )} */}
                             </div>
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900">
                                 Phone Number
                               </dt>
                               {isEditing ? (
-                                <div class="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <div className="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                   <input
                                     type="number"
                                     name="phoneNumber"
                                     id="phoneNumber"
                                     autocomplete="phoneNumber"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                     placeholder={profileData.companyPhoneNumber}
                                   />
                                 </div>
                               ) : (
-                                <dd class="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
+                                <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
                                   {profileData.companyPhoneNumber}
                                 </dd>
                               )}
                             </div>
-                            <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-                              <dt class="text-sm font-semibold leading-6 text-gray-900">
+                            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                              <dt className="text-sm font-semibold leading-6 text-gray-900">
                                 Address
                               </dt>
                               {isEditing ? (
-                                <div class="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <div className="flex sm:col-span-2 sm:mt-0 justify-self-end rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                   <input
                                     type="text"
                                     name="address"
                                     id="address"
                                     autocomplete="address"
-                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                     placeholder={profileData.address}
                                   />
                                 </div>
                               ) : (
-                                <dd class="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
+                                <dd className="mt-1 text-sm leading-6 sm:col-span-2 sm:mt-0 justify-self-end">
                                   {profileData.address}
                                 </dd>
                               )}
