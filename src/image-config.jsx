695f60e825f69e866d6013f272e6726c434618f8
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-11-07 00:18:13 +0800

    not yet finished in about

--- src/image-config.jsx
+++ src/image-config.jsx
@@ -1,4 +1,4 @@
 /** @format */
 
-const image_endpoint = "http://192.168.1.23:8000";
+const image_endpoint = "http://192.168.1.15:8000";
 export default image_endpoint;
--- src/screens/LandingScreen/About.jsx
+++ src/screens/LandingScreen/About.jsx
@@ -21,35 +21,27 @@ const About = () => {
 
         <img src={BeansLogo} alt="BeansLogo" className="h-80 w-80 mt-20" />
 
-        <div className="text-center poppins-font text-white">
-          <h1
-            // style={{ color: "white", fontFamily: "Poppins, sans-serif" }}
-            className="mt-1 mb-14 font-bold md:text-[40px] text-[30px]"
-          >
+        <div className="text-center poppins-font text-white ">
+          <h1 className="mt-1 mb-14 font-bold md:text-[40px] text-[30px]">
             About Arabica Coffee Beans Sorter
           </h1>
-          <h3
-            // style={{ color: "white", fontFamily: "Poppins, sans-serif" }}
-            className="mb-12 md:text-[18px] text-[16px] md:ml-20 md:mr-20 ml-22 mr-22"
-          >
+          <h3 className="text-justify poppins-font mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7] bg-yellow-500 bg-opacity-30 shadow-lg">
             The Arabica coffee beans sorter is a sophisticated machine designed
-            to separate coffee beans based on their size, shape, and color. This
-            sorting process is essential to remove defective or damaged beans,
-            foreign materials, and any other impurities that might have been
-            mixed with the coffee cherries during harvesting or processing.
+            to separate coffee beans based on their size, quality, and color.
+            This sorting process is essential to remove defective or damaged
+            beans, foreign materials, and any other impurities that might have
+            been mixed with the coffee cherries during harvesting or processing.
           </h3>
-          <h3
-            // style={{ color: "white", fontFamily: "Poppins, sans-serif" }}
-            className=" md:text-[18px] text-[16px] md:ml-14 md:mr-14 ml-22 mr-22 mb-10"
-          >
-            The sorter uses advanced technology, such as optical sensors, to
-            identify and categorize the beans accurately. As the coffee beans
-            pass through the machine, the sensors detect variations in size,
-            color, and shape, and based on predetermined criteria, the machine
-            sorts the beans into different grades or quality levels. This
-            ensures that only the highest-quality beans are used for roasting
-            and brewing, while lower-grade beans can be used for other purposes
-            or blends.
+
+          <h3 className="text-justify poppins-font mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7]">
+            The sorter uses advanced technology, such as high maginification
+            camera, to identify and categorize the beans accurately. As the
+            coffee beans pass through the machine, the cameras detect variations
+            in size, color, and quality, and based on predetermined criteria,
+            the machine sorts the beans into different grades or quality levels.
+            This ensures that only the highest-quality beans are used for
+            roasting and brewing, while lower-grade beans can be used for other
+            purposes or blends.
           </h3>
         </div>
       </div>
