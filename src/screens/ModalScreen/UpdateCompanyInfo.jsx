bd6177be6fa78bb3f3b2efbca4a54b16c2382569
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-07 19:31:48 +0800

    profile image wont save to database

--- src/screens/ModalScreen/UpdateCompanyInfo.jsx
+++ src/screens/ModalScreen/UpdateCompanyInfo.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import api_endpoint from "../../config";
 import Modal from "../../component/Modal";
+import { useForm } from "react-hook-form";
 
 const imageMimeType = /image\/(png|jpg|jpeg)/i;
 
 const UpdateCompanyInfo = ({ show, onClose }) => {
     const [modalStep, setModalStep] = useState(1); // Track the current step of the modal
+    const [loading, setLoading] = useState(false);
     const [companyName, setCompanyName] = useState("");
     const [companyAddress, setCompanyAddress] = useState("");
     const [companyPhoneNumber, setCompanyPhoneNumber] = useState("");
@@ -14,6 +16,11 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
     // const [closeModal, setCloseModal] = useState(onClose);
     const [profileFileDataURL, setProfileFileDataURL] = useState(null);
     const [companyFileDataURL, setCompanyFileDataURL] = useState(null);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
 
     const profileImageHandler = (e) => {
@@ -85,114 +92,37 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
         setModalStep((step) => step - 1);
     };
 
-    const updateCustomerDetails = async (e) => {
-        e.preventDefault();
-        try {
-            let token = localStorage.getItem("token");
-            let user_id = localStorage.getItem("user_id");
-            const currentDate = new Date().toISOString();
-
-            const response = await fetch(api_endpoint + "/edit-customer/" + customerId, {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: "Bearer " + token,
-                },
-                body: JSON.stringify({
-                    customerName: newCustomerName,
-                    phoneNum: newCustomerPhoneNumber,
-                    address: newCustomerAddress,
-                    //kiloOfBeans: newCustomerKiloOfBeans,
-                    registrationDate: currentDate,
-                    // year: selectedYearValue, // Use the selected year
-                    // month: selectedMonthValue, // Use the selected month value
-                }),
-            });
-            if (response.status === 422) {
-                alert("Customer is already in the database");
-            }
-            if (!response.ok) {
-                throw new Error("Fail to add customer");
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
-        closeModal();
-    };
-
-    const handleSubmitCompanyInfo = async (e) => {
-        // You can send the company info to the server here
-        // Update user's company info in the database
-        // Close the modal and update the state accordingly
-        // setCompanyInfoMissing(false);
-        // setModalStep(1);
-        // setUserInfo(prevUserInfo => ({
-        //   ...prevUserInfo,
-        //   company: {
-        //     name: companyName,
-        //     address: companyAddress,
-        //   },
-        // }));
-        // You might want to make an API call to update the company info in the database
-    };
-
-    const handleProfileImageChange = (e) => {
-        setProfileImage(e.target.files[0]);
-    };
-
-    const handleCompanyImageChange = (e) => {
-        setCompanyImage(e.target.files[0]);
-    };
-
-    const handleUpdateCompanyImage = async (e) => {
-        try {
-            if (!companyImage) {
-                return;
-            }
-
-            const formData = new FormData();
-            formData.append("image", companyImage);
-
-            const response = await axios.post(api_endpoint + "/fetch-info", formData, {
+    const onSubmitHandler = (data) => {
+        setLoading(true);
+        const formData = new FormData()
+        formData.append("user_id", user_id)
+        formData.append("companyName", data.companyName)
+        formData.append("companyNumber", data.companyNumber)
+        formData.append("companyLocation", data.companyLocation)
+        formData.append("profileAvatar", profileImage)
+        formData.append("images", companyImage)
+        axios
+            .post(api_endpoint + "/add-info", data, {
                 headers: {
                     "Content-Type": "multipart/form-data",
+                    Authorization: 'Bearer ' + token
                 },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    setTimeout(() => {
+                        setLoading(false);
+                        closeModal();
+                      }, 2000); 
+                }
+            })
+            .catch((error) => {
+                console.error("Error", error.response.data);
+            })
+            .finally(() => {
+                setLoading(false); // Reset loading state
             });
-
-            if (response.status === 200) {
-                // Profile image updated successfully
-                // You can update the user's profile image state here if needed
-                console.log("Company image updated");
-            }
-        } catch (error) {
-            console.error("Error updating company image:", error);
-        }
-    };
-
-    const handleUpdateProfileImage = async (e) => {
-        try {
-            if (!profileImage) {
-                return;
-            }
-
-            const formData = new FormData();
-            formData.append("image", profileImage);
-
-            const response = await axios.post(api_endpoint + "/user", formData, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            });
-
-            if (response.status === 200) {
-                // Profile image updated successfully
-                // You can update the user's profile image state here if needed
-                console.log("Profile image updated");
-            }
-        } catch (error) {
-            console.error("Error updating profile image:", error);
-        }
+        //console.log(data);
     };
 
     return (
@@ -202,9 +132,10 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
             className="modal fixed inset-0 flex items-center justify-center z-50"
             overlayClassName="absolute inset-0 bg-black opacity-50"
         >
+            <form onSubmit={handleSubmit(onSubmitHandler)}>
             <div className="rounded max-w-sm mx-auto z-50">
                 <span
-                    className="modal-close absolute top-4 right-4 text-xl cursor-pointer"
+                    className="modal-close bg-white absolute pl-5 pr-5 top-4 right-4 text-xl cursor-pointer"
                     onClick={closeModal}
                 >
                     &times;
@@ -294,8 +225,21 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
                             type="text"
                             value={companyName}
                             onChange={(e) => setCompanyName(e.target.value)}
-                            className="border rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400"
+                            {...register("companyName", {
+                                required: "Company Name is required",
+                                pattern: {
+                                    value: /^[a-zA-Z ]{2,30}$/,
+                                    message: "Invalid Company Name",
+                                },
+                            })}
+                            className={`bg-white w-full rounded-[10px] h-10 text-black px-4 ${errors.companyName ? "mb-2" : "mb-5"
+                                }`}
+                            style={{ fontFamily: "Poppins, sans-serif" }}
+                            // className="border rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400"
                         />
+                        {errors.companyName && (
+                                <p className="text-red-500 ml-2">{errors.companyName.message}</p>
+                        )}
                         <div className="flex justify-center mt-4">
                             <button
                                 onClick={handlePreviousStep}
@@ -322,8 +266,20 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
                             type="text"
                             value={companyPhoneNumber}
                             onChange={(e) => setCompanyPhoneNumber(e.target.value)}
-                            className="border rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400"
+                            {...register("companyNumber", {
+                                required: "Phone Number is required",
+                                pattern: {
+                                    value: /^[\d-()+\s]*$/,
+                                    message: "Invalid Phone Number",
+                                }
+                            })}
+                            className={`bg-white w-full rounded-[10px] h-10 text-black px-4 ${errors.companyNumber ? "mb-2" : "mb-5"
+                                }`}
+                            style={{ fontFamily: "Poppins, sans-serif" }}
                         />
+                        {errors.companyNumber && (
+                            <p className="text-red-500 ml-2">{errors.companyNumber.message}</p>
+                        )}
                         <div className="flex justify-center mt-4">
                             <button
                                 onClick={handlePreviousStep}
@@ -350,8 +306,21 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
                             type="text"
                             value={companyAddress}
                             onChange={(e) => setCompanyAddress(e.target.value)}
-                            className="border rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400"
+                            {...register("companyLocation", {
+                                required: "Location is required",
+                                pattern: {
+                                    value: /^[a-zA-Z ]{2,30}$/,
+                                    message: "Invalid Location",
+                                },
+                            })}
+                            className={`bg-white w-full rounded-[10px] h-10 text-black px-4 ${errors.companyLocation ? "mb-2" : "mb-5"
+                                }`}
+                            style={{ fontFamily: "Poppins, sans-serif" }}
                         />
+
+                        {errors.companyLocation && (
+                            <p className="text-red-500 ml-2">{errors.companyLocation.message}</p>
+                        )}
                         <div className="flex justify-center mt-4">
                             <button
                                 onClick={handlePreviousStep}
@@ -360,15 +329,37 @@ const UpdateCompanyInfo = ({ show, onClose }) => {
                                 Previous
                             </button>
                             <button
-                                onClick={handleSubmitCompanyInfo}
+                                onClick={handleSubmit(onSubmitHandler)}
+                                disabled={loading}
                                 className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded focus:outline-none"
                             >
-                                Submit
+                                {loading ? (
+                                    <svg
+                                        className="animate-spin h-5 w-5 mr-3 text-white"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.96 8.01 0 014 12H0c0 3.042 1.135 5.86 3.169 8.022l2.83-2.73zM12 20a8 8 0 008-8h4a12 12 0 01-12 12v-4zm5.819-10.169A7.96 8.01 0 0120 12h4c0-3.042-1.135-5.86-3.169-8.022l-2.83 2.73z"
+                                        ></path>
+                                    </svg>
+                                ) : null}
+                                {loading ? "Processing..." : "Submit"}
                             </button>
                         </div>
                     </>
                 )}
             </div>
+            </form>
         </Modal>
     );
 };
