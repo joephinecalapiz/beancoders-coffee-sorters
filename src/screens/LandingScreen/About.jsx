65ed9cc88fba18044aaf8c26b277db72e6593ef7
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-08 11:30:20 +0800

    update code

--- src/screens/LandingScreen/About.jsx
+++ src/screens/LandingScreen/About.jsx
@@ -16,16 +16,15 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col justify-center items-center bg-CoffeeBeans bg-cover min-h-screen w-full">
+      <div className="flex flex-col justify-center items-center bg-CoffeeBeans bg-cover h-full w-full">
         {/* Use min-h-screen to make sure the div takes at least the height of the screen */}
 
         <img src={BeansLogo} alt="BeansLogo" className="h-80 w-80 mt-20" />
-
-        <div className="text-center poppins-font text-white ">
-          <h1 className="mt-1 mb-14 font-bold md:text-[40px] text-[30px]">
+        <div className="text-center poppins-font text-white">
+          <h1 className="mt-1 mb-14 font-bold md:text-[40px] text-[30px] z-50">
             About Arabica Coffee Beans Sorter
           </h1>
-          <h3 className="text-justify poppins-font mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7]">
+          <h3 className="text-justify mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7]">
             The Arabica coffee beans sorter is a sophisticated machine designed
             to separate coffee beans based on their size, quality, and color.
             This sorting process is essential to remove defective or damaged
@@ -33,7 +32,7 @@ const About = () => {
             been mixed with the coffee cherries during harvesting or processing.
           </h3>
 
-          <h3 className="text-justify poppins-font mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7]">
+          <h3 className="text-justify mb-12 md:text-[18px] text-[16px] m-5 md:ml-20 md:mr-20 ml-22 mr-22 md:leading-[1.7]">
             The sorter uses advanced technology, such as high maginification
             camera, to identify and categorize the beans accurately. As the
             coffee beans pass through the machine, the cameras detect variations
