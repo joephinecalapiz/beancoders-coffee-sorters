22b9fcb5abb1fc4990660f066f77e369c2de637c
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-11-13 00:39:43 +0800

    update landing page

--- src/screens/LandingScreen/Landing.jsx
+++ src/screens/LandingScreen/Landing.jsx
@@ -44,99 +44,113 @@ const Landing = () => {
   return (
     <>
       <Navbar />
-      <div className="mt-12 bg-BgLanding md:bg-cover bg-cover bg-opacity-20 h-screen w-full">
-        <div className="text-white font-bold text-2xl text-center py-4 md:py-16 flex flex-col items-center">
-          <div className="md:ml-4 mt-1">
-            <img src={BeansLogo} alt="BeansLogo" className="h-80 w-80 mt-5" />
-          </div>
-          <div className="text-center md:ml-32 px-4">
-            <p className="text-4xl mb-4 md:text-5xl md:mb-12 font-poppins md:block hidden poppins-font">
-              BeanCoders:
-            </p>
-            <p className=" mb-4 text-4xl md:text-6xl md:ml-96 poppins-font">
-              Quality Bean Sorter
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-black">
-        <div className="text-center justify-center items-center">
-          <h1 className="text-white mb-8 md:mb-20 poppins-font text-3xl md:text-5xl mt-0 font-bold">
-            Coffee Sorting Establishments
-          </h1>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center items-center mb-14 mx-4">
-          {companyData.length > 0 ? (
-            companyData.map((detail, index) => (
-              <div
-                key={index}
-                className="bg-white md:min-h-[250px] md:w-65 rounded-lg p-2 flex flex-col relative shadow-4xl z-100 md:flex-row"
-              >
-                {/* Image Section */}
-                {/* <div className="md:w-[50%] md:absolute"> */}
-                <div className="md:w-1/2 md:flex-shrink-0 md:relative md:h-full">
-                  {detail && detail.images ? (
-                    <img
-                      src={`${image_endpoint}/storage/${detail.images}`}
-                      alt="beansLogo"
-                      className="w-70 h-64 md:w-70 md:h-64 max-h-full max-w-full mb-4 items-center  "
-                    />
-                  ) : (
-                    <div>No image available</div>
-                  )}
+      <div className="block">
+        <div className="bg-BgLanding bg-fixed md:bg-cover bg-cover bg-opacity-20 md:h-screen w-full sm:bg-scroll">
+          <div className="text-white poppins-font font-bold text-2xl py-[5%] flex flex-col items-center md:items-end">
+            <div className="mt-10 md:mt-1">
+              <div className="px-6 md:mr-20">
+                <div>
+                  <img
+                    src={BeansLogo}
+                    alt="BeansLogo"
+                    className="md:h-80 md:w-80 h-30 w-30 mt-8 md:mt-0 "
+                  />
+                  <p className="ml-26 poppins-font text-2xl block text-center md:hidden">
+                    Arabica Coffee Beans Sorter
+                  </p>
                 </div>
 
-                {/* Details Section */}
-                <div className="md:pl-6 md:order-2 md:w-50">
-                  {detail && detail.companyName ? (
-                    <div className="text-black poppins-font  dark:text-textTitle font-bold text-3xl mt-3 flex items-center">
-                      <FontAwesomeIcon icon={faBuilding} className="mr-10" />
-                      <span className="flex-grow">{detail.companyName}</span>
-                    </div>
-                  ) : (
-                    <div>No data available</div>
-                  )}
-
-                  {detail && (
-                    <div className="text-black poppins-font dark:text-textDesc text-3xl mt-5 flex items-center">
-                      <FontAwesomeIcon
-                        icon={faMapMarkerAlt}
-                        className="mr-10"
-                      />
-                      <a
-                        href={`https://www.google.com/maps/search/?q=${encodeURIComponent(
-                          detail.companyLocation
-                        )}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="flex-grow cursor-pointer"
-                      >
-                        {detail.companyLocation}
-                      </a>
-                    </div>
-                  )}
-
-                  {detail && (
-                    <div className="text-black poppins-font dark:text-textDesc text-3xl font-poppins mt-5 flex items-center">
-                      <FontAwesomeIcon icon={faPhone} className="mr-10" />
-                      <span className="flex-grow">{detail.companyNumber}</span>
-                    </div>
-                  )}
-                </div>
+                <p className="md:ml-36 poppins-font md:text-5xl md:mb-10 md:block hidden">
+                  Bean Coders:
+                </p>
+                <p className="md:ml-36 poppins-font text-4xl md:block hidden md:text-6xl">
+                  Quality Bean Sorter
+                </p>
               </div>
-            ))
-          ) : (
-            <div className="col-span-4 text-center">
-              No company data available.
             </div>
-          )}
+          </div>
         </div>
+        <div className="bg-black">
+          <div className="text-center justify-center items-center">
+            <h1 className="text-white mb-8 md:mb-20 poppins-font text-3xl md:text-5xl mt-0 font-bold">
+              Coffee Sorting Establishments
+            </h1>
+          </div>
 
-        <div className="flex items-center"></div>
-        <br />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center items-center mb-14 mx-4">
+            {companyData.length > 0 ? (
+              companyData.map((detail, index) => (
+                <div
+                  key={index}
+                  className="bg-white md:min-h-[250px] md:w-65 rounded-lg p-2 flex flex-col relative shadow-4xl z-100 md:flex-row"
+                >
+                  {/* Image Section */}
+                  {/* <div className="md:w-[50%] md:absolute"> */}
+                  <div className="md:w-1/2 md:flex-shrink-0 md:relative md:h-full">
+                    {detail && detail.images ? (
+                      <img
+                        src={`${image_endpoint}/storage/${detail.images}`}
+                        alt="beansLogo"
+                        className="w-70 h-64 md:w-70 md:h-64 max-h-full max-w-full mb-4 items-center  "
+                      />
+                    ) : (
+                      <div>No image available</div>
+                    )}
+                  </div>
+
+                  {/* Details Section */}
+                  <div className="md:pl-6 md:order-2 md:w-50">
+                    {detail && detail.companyName ? (
+                      <div className="text-black poppins-font  dark:text-textTitle font-bold text-3xl mt-3 flex items-center">
+                        <FontAwesomeIcon icon={faBuilding} className="mr-10" />
+                        <span className="flex-grow">{detail.companyName}</span>
+                      </div>
+                    ) : (
+                      <div>No data available</div>
+                    )}
+
+                    {detail && (
+                      <div className="text-black poppins-font dark:text-textDesc text-3xl mt-5 flex items-center">
+                        <FontAwesomeIcon
+                          icon={faMapMarkerAlt}
+                          className="mr-10"
+                        />
+                        <a
+                          href={`https://www.google.com/maps/search/?q=${encodeURIComponent(
+                            detail.companyLocation
+                          )}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="flex-grow cursor-pointer"
+                        >
+                          {detail.companyLocation}
+                        </a>
+                      </div>
+                    )}
+
+                    {detail && (
+                      <div className="text-black poppins-font dark:text-textDesc text-3xl font-poppins mt-5 flex items-center">
+                        <FontAwesomeIcon icon={faPhone} className="mr-10" />
+                        <span className="flex-grow">
+                          {detail.companyNumber}
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div className="col-span-4 text-center">
+                No company data available.
+              </div>
+            )}
+          </div>
+
+          <div className="flex items-center"></div>
+          <br />
+        </div>
       </div>
+
       <Footer></Footer>
     </>
   );
