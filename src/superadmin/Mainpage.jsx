52c08035a55f7f8bf0254322709fb93226a20785
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-12 13:54:00 +0800

    added main component to super admin

---
+++ src/superadmin/Mainpage.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import AdminSidebar from "../component/AdminSidebar";
+import AdminTop from "../component/AdminTopbar";
+
+function Main({ children }) {
+    const [navVisible, showNavbar] = useState(false);
+
+    const toggleSidebar = () => {
+        showNavbar(!navVisible);
+    };
+
+    return (
+        <>
+            <AdminTop onToggleSidebar={toggleSidebar}
+                collapsed={navVisible}
+                handleToggleSidebar={toggleSidebar} />
+            <AdminSidebar collapsed={navVisible} handleToggleSidebar={toggleSidebar} />
+            <div className={`main relative z-5 inset-0 mt-16  ${!navVisible ? "sm:ml-60" : "ml-16"}`}
+                style={{
+                    transition: "margin-left 0.3s ease",
+                }}
+            >
+                {children}
+            </div>
+        </>
+    )
+}
+
+export default Main
