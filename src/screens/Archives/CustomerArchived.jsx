d49d73ff67bdf11a986ed457b9d28cd7447b88c2
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-16 21:38:11 +0800

    update icon

--- src/screens/Archives/CustomerArchived.jsx
+++ src/screens/Archives/CustomerArchived.jsx
@@ -107,9 +107,11 @@ const CustomerArchived = () => {
       }
       // Update customer data after successful archive
       fetchCustomers();
+      setOpenDropdownId(null);
     } catch (error) {
       console.error("Error deleting or fetching customer data:", error);
     }
+    setOpenDropdownId(null);
   };
 
   useEffect(() => {
@@ -335,10 +337,12 @@ const CustomerArchived = () => {
                                       onClick={() =>
                                         deleteCustomer(customer.id)
                                       }
-                                      className="block px-4 py-2 mx-auto hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                    >
-                                      Permanent Delete
-                                    </button>
+                                      className="poppins-font flex items-center justify-center px-4 py-2 mx-auto hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                  >
+                                    <span className="delete pr-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>                                    </span>
+                                    Permanent Delete
+                                  </button>
                                   </li>
                                 </ul>
                               </div>
