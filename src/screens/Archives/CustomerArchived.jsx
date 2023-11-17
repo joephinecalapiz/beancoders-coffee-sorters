33d3ce7ef337a8f7385b5e93ad7284d0026694ed
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-11-17 18:06:22 +0800

    update

--- src/screens/Archives/CustomerArchived.jsx
+++ src/screens/Archives/CustomerArchived.jsx
@@ -1,11 +1,9 @@
 /** @format */
 import React, { useState, useEffect } from "react";
-import "../.././css/customer.css";
-import "../.././css/Sidebar.css";
+import ".././css/customer.css";
+import ".././css/Sidebar.css";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
-import Topbar from "../../component/Topbar";
-import Sidebar from "../../component/Sidebar";
 import api_endpoint from "../../config";
 
 const CustomerArchived = () => {
@@ -338,11 +336,19 @@ const CustomerArchived = () => {
                                         deleteCustomer(customer.id)
                                       }
                                       className="poppins-font flex items-center justify-center px-4 py-2 mx-auto hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                                  >
-                                    <span className="delete pr-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>                                    </span>
-                                    Permanent Delete
-                                  </button>
+                                    >
+                                      <span className="delete pr-2">
+                                        <svg
+                                          xmlns="http://www.w3.org/2000/svg"
+                                          height="24"
+                                          viewBox="0 -960 960 960"
+                                          width="24"
+                                        >
+                                          <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+                                        </svg>{" "}
+                                      </span>
+                                      Permanent Delete
+                                    </button>
                                   </li>
                                 </ul>
                               </div>
