a13dbb2065a69514d214e7c0c4c07b3ab1800a87
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-18 20:43:17 +0800

    redux store

---
+++ redux/store.js
@@ -0,0 +1,12 @@
+// store.js
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    // Add other reducers as needed
+  },
+});
+
+export default store;
