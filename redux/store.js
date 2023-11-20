7d488de418fc1f78891255565f4eeda8a42e493e
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-20 11:51:01 +0800

    store user and company details

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,14 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import companyReducer from './companySlice';
+import userReducer from './userSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    company: companyReducer,
+    user: userReducer,
     // Add other reducers as needed
   },
 });
