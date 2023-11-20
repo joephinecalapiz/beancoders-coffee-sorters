54afb64024a734cc5b0e3361b6d0e9830b927795
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-20 11:49:40 +0800

    company details

---
+++ redux/companySlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchCompanyDetails } from './userActions';
+
+// initialize role from local storage
+const storedCompanyInfo = JSON.parse(sessionStorage.getItem('companyInfo'))
+  ? JSON.parse(sessionStorage.getItem('companyInfo'))
+  : null
+
+const initialState = {
+    companyInfo: storedCompanyInfo,
+    status: 'idle',
+    error: null,
+};
+
+const companySlice = createSlice({
+    name: 'company',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCompanyDetails.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchCompanyDetails.fulfilled, (state, action) => {
+                state.status = 'succeeded';
+                state.companyInfo = action.payload;
+            })
+            .addCase(fetchCompanyDetails.rejected, (state, action) => {
+                state.status = 'failed';
+                state.error = action.error.message;
+            });
+    },
+});
+
+export default companySlice.reducer;
