af517c8f77a9f54bef4a69b00eadeeab5d0b57a7
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-24 09:19:53 +0800

    remove companySlice

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
-import companyReducer from './companySlice';
 import userReducer from './userSlice';
 import { authApi } from './authService'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    company: companyReducer,
     user: userReducer,
     [authApi.reducerPath]: authApi.reducer,
     // Add other reducers as needed
