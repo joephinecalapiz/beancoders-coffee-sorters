213a33eeb2d305205dd6d840a0539d9c7ae6f314
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-11-27 15:52:36 +0800

    adde customer reducer

--- redux/store.js
+++ redux/store.js
@@ -2,12 +2,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './services/auth/authSlice';
 import userReducer from './services/user/userSlice';
-import { authApi } from './services/auth/authService'
+import customerReducer from './services/customer/customerSlice';
+import { authApi } from './services/api/authService'
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    customer: customerReducer,
     [authApi.reducerPath]: authApi.reducer,
     // Add other reducers as needed
   },
