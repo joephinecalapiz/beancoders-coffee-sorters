dd0e080c5dac619d47840576b67d59277fcc4113
Jeremiah Larry King Ungsod <67980301+justjelku@users.noreply.github.com>
2023-12-08 13:11:03 +0800

    update

--- redux/services/user/userSlice.js
+++ redux/services/user/userSlice.js
@@ -1,17 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchSorterInfo, fetchStatusInfo, fetchUserDetails } from './userActions';
+import { fetchUserDetails } from './userActions';
 
 // initialize role from local storage
 const userInfo = JSON.parse(sessionStorage.getItem('userInfo'))
     ? JSON.parse(sessionStorage.getItem('userInfo'))
-    : []
-
-const sorters = JSON.parse(sessionStorage.getItem('sorterData'))
-    ? JSON.parse(sessionStorage.getItem('sorterData'))
-    : []
-
-const statusInfo = JSON.parse(sessionStorage.getItem('statusData'))
-    ? JSON.parse(sessionStorage.getItem('statusData'))
     : []
 
 // console.log(customers)
@@ -20,8 +12,6 @@ const statusInfo = JSON.parse(sessionStorage.getItem('statusData'))
 const initialState = {
     userInfo,
     companyInfo: '',
-    sorters,
-    statusInfo,
     status: 'idle',
     error: null,
 };
@@ -42,30 +32,6 @@ const userInfoSlice = createSlice({
                 state.companyInfo = action.payload.details[0];
             })
             .addCase(fetchUserDetails.rejected, (state, action) => {
-                state.status = 'failed';
-                state.error = action.error.message;
-            })
-            //sorter
-            .addCase(fetchSorterInfo.pending, (state) => {
-                state.status = 'loading';
-            })
-            .addCase(fetchSorterInfo.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                state.sorters = action.payload.sorters;
-            })
-            .addCase(fetchSorterInfo.rejected, (state, action) => {
-                state.status = 'failed';
-                state.error = action.error.message;
-            })
-            //status
-            .addCase(fetchStatusInfo.pending, (state) => {
-                state.status = 'loading';
-            })
-            .addCase(fetchStatusInfo.fulfilled, (state, action) => {
-                state.status = 'succeeded';
-                state.statusInfo = action.payload.status;
-            })
-            .addCase(fetchStatusInfo.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.error.message;
             })
